ec36af34c26e0339f4095b8b740217d20ebb4725
ffghdfh <96344370+ffghdfh@users.noreply.github.com>
2024-04-23 13:23:38 +0200

    Create changelog.pp
    changelog file for updates

---
+++ changelog.pp
@@ -0,0 +1,9 @@
+Here's what's new in 1.1.2!
+A few small bug fixes:
+Fixed buttons detecting a press when they shouldn't.
+Shapes won't accitentally place while pressing a shape button in the shape menu.
+
+A few small new features:
+Line snapping: You can now make lines snap in 45 degree increments by pressing the big circle button in the shape menu.
+Palette is now always visible to make things easier.
+Brand new update notifier system!
