28fe48d125dc5c5da976e97af55ce8c3d6b5197d
ffghdfh <96344370+ffghdfh@users.noreply.github.com>
2024-04-24 14:38:33 +0200

    Update changelog.pp

--- changelog.pp
+++ changelog.pp
@@ -2,6 +2,9 @@ Here's what's new in 1.1.2!
 A few small bug fixes:
 Fixed buttons detecting a press when they shouldn't.
 Shapes won't accitentally place while pressing a shape button in the shape menu.
+Fixed a few issues that probably didn't affect regular users.
+Removed some pointless code.
+Fixed accidental debug code for saving, making you copy the save to the clipboard even on PC.
 
 A few small new features:
 Line snapping: You can now make lines snap in 45 degree increments by pressing the big circle button in the shape menu.
