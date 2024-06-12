572384cd51837710ff4f3e425ea98f51c8f93f43
ffghdfh <96344370+ffghdfh@users.noreply.github.com>
2024-06-12 18:16:41 +0200

    Update changelog.pp

--- changelog.pp
+++ changelog.pp
@@ -1,15 +1,14 @@
-Here's what's new in 1.2!
-Huge fixes:
-Pen size now matches the palette menu's pen size from the start
-Replaced some infinite loops to reduce processing load
-Behind the scenes improvements (mostly helps me lol)
-Fixed some weird undoing and redoing issues
+Here's what's new in 1.3!
+New features:
+Upgraded keyboard shortcuts, you can do almost everything with WASD now! (A - D switch between tools, W - S increase / decrease pen size when palette open, Space to open palette, Q - E undo / redo)
+Changed angle snap to shape snap, where lines snap to 45° increments, and squares / circles / triangles are uniform. (all sides are same size)
+You can now use "\n" to break text in the text tool into separate lines. When breaking up text with \n, the text will get (mostly) centered vertically. (typing in enough text to trigger automatic line breaking DOES NOT center the text yet.)
 
-Huge changes:
-New palette menu (better preview, better shape buttons, better size picker, better color picker)
-made pen preview borders make a bit more sense
-You you can show both the predefined and custom colors in palette
-Triangle tool has markers to show triangle's orientation
-Brand new settings menu (separate from palette)
-You can change which side the size slider is on (i'm left handed and was
-a bit selfish when making the app so now right handed ppl can use it better)
+Bug fixes:
+Fixed the warning screen popping up before turning black and fading back in.
+Fixed text tool being flipped by default.
+Fixed drawing tool going off when tapping palette button on touchscreen devices. (NOTE!! This introduces a slight delay when drawing! Depends on your framerate on how long it is. Sorry! Tip: try not to make very very quick strokes or half of it might get cut off.)
+
+Misc:
+Fixed updater screen to include shorter bitly link.
+Added a short and frankly ugly keyboard shortcut explanation in the tutorial.
