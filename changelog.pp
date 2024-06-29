d718b2d77dc2b3a4558ecfe449e0e21a57957b3f
ffghdfh <96344370+ffghdfh@users.noreply.github.com>
2024-06-29 12:56:43 +0200

    Update changelog.pp

--- changelog.pp
+++ changelog.pp
@@ -1,15 +1,11 @@
-Here's what's new in 1.3!
-New features:
-Upgraded keyboard shortcuts, you can do almost everything with WASD now! (A - D switch between tools, W - S increase / decrease pen size when palette open, Space to open palette, Q - E undo / redo)
-Changed angle snap to shape snap, where lines snap to 45° increments, and squares / circles / triangles are uniform. (all sides are same size)
-You can now use "\n" to break text in the text tool into separate lines. When breaking up text with \n, the text will get (mostly) centered vertically. (typing in enough text to trigger automatic line breaking DOES NOT center the text yet.)
+EMERGENCY BUGFIX UPDATE!!
+Here's what's new in 1.3.1!
 
 Bug fixes:
-Fixed the warning screen popping up before turning black and fading back in.
-Fixed text tool being flipped by default.
-Fixed drawing tool going off when tapping palette button on touchscreen devices. (NOTE!! This introduces a slight delay when drawing! Depends on your framerate on how long it is. Sorry! Tip: try not to make very very quick strokes or half of it might get cut off.)
-Update changelog text now starts off at the correct Y position.
+Fixed a coding error that handles uniform square creation, so now it's way easier to change the direction of the square / triangle / circle.
+Fixed using A / D keys not updating the left toolbar.
 
-Misc:
-Fixed updater screen to include shorter bitly link.
-Added a short and frankly ugly keyboard shortcut explanation in the tutorial.
+Changes:
+Changed palette slider to increment by 0.5 for finer pen size control.
+Changed warning screen to reflect the ACTUAL issue with saving / loading.
+Removed loading text + New loading screen image :) (made in the app)
