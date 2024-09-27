257b1a026c9a633927a54cb8288be36b692445d2
ffghdfh <96344370+ffghdfh@users.noreply.github.com>
2024-09-27 15:43:00 +0200

    Update changelog.pp

--- changelog.pp
+++ changelog.pp
@@ -1,11 +1,23 @@
-EMERGENCY BUGFIX UPDATE!!
-Here's what's new in 1.3.1!
+HUGE 1.4 UPDATE! VERY IMPORTANT!!
+FIXES A LOT OF WEIRD ISSUES! Also sorry, but for
+now, only PC will get releases.
+In turn, ARM builds are available for both
+windows and linux.
+fixes:
+- fixed weird initial pen size AND eraser size (i think?)
+- version number doesn't show in tutorial anymore
+- fixed weird cog icon and arrow to not be blue
+- fixed half visible triangle preview
+- mostly fixed the random line bug, try drawing a bit slower if it happens
+(- changed splash screen to reflect random line fix)
+- finally fixed the full screen popups that wouldn't appear while undoing and redoing
 
-Bug fixes:
-Fixed a coding error that handles uniform square creation, so now it's way easier to change the direction of the square / triangle / circle.
-Fixed using A / D keys not updating the left toolbar.
+new stuff:
+- added rainbow pens
+- added pen preview
+- more accurate pen stroke saving, at the cost of lag
 
-Changes:
-Changed palette slider to increment by 0.5 for finer pen size control.
-Changed warning screen to reflect the ACTUAL issue with saving / loading.
-Removed loading text + New loading screen image :) (made in the app)
+behind the scenes stuff:
+- changed spray pen's spraying method to be less cpu intensive
+- minor performance improvements, such as less infinite loops,
+more "wait until"-s, pen checks if touching canvas, not UI
