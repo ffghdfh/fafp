a6fe5e8cde9d751daa94f31336d51fc7a85949c1
ffghdfh <96344370+ffghdfh@users.noreply.github.com>
2024-10-16 17:13:34 +0200

    Update changelog.pp

--- changelog.pp
+++ changelog.pp
@@ -1,23 +1,15 @@
-HUGE 1.4 UPDATE! VERY IMPORTANT!!
-FIXES A LOT OF WEIRD ISSUES! Also sorry, but for
-now, only PC will get releases.
-In turn, ARM builds are available for both
-windows and linux.
-fixes:
-- fixed weird initial pen size AND eraser size (i think?)
-- version number doesn't show in tutorial anymore
-- fixed weird cog icon and arrow to not be blue
-- fixed half visible triangle preview
-- mostly fixed the random line bug, try drawing a bit slower if it happens
-(- changed splash screen to reflect random line fix)
-- finally fixed the full screen popups that wouldn't appear while undoing and redoing
+Here's what's new in 1.4.1! (Please update if you're on
+anything lower than 1.4, 1.3 sucks.
+NO MORE INSTALLERS! Just the zip containing the game files.
+Both chrome and windows flags the 64 bit installer.
 
-new stuff:
-- added rainbow pens
-- added pen preview
-- more accurate pen stroke saving, at the cost of lag
+Bug fixes:
+- Fixed rainbow text not loading as rainbow
+- Fixed text undoing deleting more than one text
 
-behind the scenes stuff:
-- changed spray pen's spraying method to be less cpu intensive
-- minor performance improvements, such as less infinite loops,
-more "wait until"-s, pen checks if touching canvas, not UI
+New stuff:
+- added automatic reload feature
+- redid the title screen a bit to fit UI theme
+
+Misc:
+- Fixed the version number on the title screen
